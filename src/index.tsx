716be8f23b313615ce825e4445ddaf0b64009b02
en0c-026 <bitup.games@gmail.com>
2021-07-15 20:57:01 -0300

    update theme setup

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,89 @@ startRouter(routes, store, {
 });
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFFFFF',
+    },
+    secondary: {
+      main: '#747474',
+    },
+    warning: {
+      main: '#F2A627',
+    },
+  },
   typography: {
-    fontFamily: '"IBM Plex Mono", "IBM Plex Sans", mono space, sans-serif',
+    fontFamily: '"IBM Plex Sans", sans-serif',
+    h1: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '32px',
+      lineHeight: '40px',
+    },
+    h2: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '20px',
+      lineHeight: '28px',
+    },
+    h3: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '20px',
+      lineHeight: '28px',
+    },
+    h4: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    h5: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    h6: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    subtitle1: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '13px',
+      lineHeight: '21px',
+    },
+    subtitle2: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '13px',
+      lineHeight: '16.9px',
+    },
+    caption: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '24px',
+      lineHeight: '40px',
+    },
+    overline: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '9px',
+      letterSpacing: '1px',
+    },
   },
 });
 
