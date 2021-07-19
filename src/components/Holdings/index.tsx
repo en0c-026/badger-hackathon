94bc814209bd674040d3171bd35c59536bb4ba76
en0c-026 <bitup.games@gmail.com>
2021-07-19 07:02:56 -0300

    update main component of the Holdings section, contains secondary components

--- src/components/Holdings/index.tsx
+++ src/components/Holdings/index.tsx
@@ -1,24 +1,58 @@
-import { makeStyles, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles, Grid, Paper } from '@material-ui/core';
+import BadgerRewards from './BadgerRewards';
+import EarningsValue from './EarningsValue';
+import { SettSelector } from './SettSelector';
+import SetupChartButtons from './SetupChartButtons';
 
-const useStyleHoldings = makeStyles(() => ({
+export const useStyleHoldings = makeStyles(() => ({
   rootCard: {
     minHeight: '696px',
     marginBottom: '24px',
-    padding: '32px 24px',
+    padding: '32px 26px',
     color: '#FFFFFF',
     backgroundColor: '#2B2B2B',
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     borderRadius: '16px',
   },
+  buttonModal: {
+    color: '#FFFFFF',
+    backgroundColor: '#101010',
+    textTransform: 'none',
+    height: '100%',
+    padding: '8px 12px',
+  },
+  gridButtons: {
+    marginTop: '20px',
+    height: '40px',
+  },
 }));
+
+const defaultState: Array<boolean> = [false, false, false, false, false, false, false, false, false];
 
 const Holdings = (): JSX.Element => {
   const classes = useStyleHoldings();
+  const [timeframe, setTimeFrame] = useState<string>('1D');
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [checkboxs, setCheckboxs] = useState<Array<boolean>>(defaultState);
+
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+
   return (
-    <div className={classes.rootCard}>
-      <Typography>asd</Typography>
-    </div>
+    <Paper className={classes.rootCard}>
+      <Grid container spacing={3} direction="column">
+        <EarningsValue />
+        <BadgerRewards />
+        <SetupChartButtons timeframe={timeframe} setTimeFrame={setTimeFrame} handleOpenModal={handleOpenModal} />
+      </Grid>
+      <SettSelector
+        openModal={openModal}
+        handleCloseModal={handleCloseModal}
+        checkboxs={checkboxs}
+        setCheckboxs={setCheckboxs}
+      />
+    </Paper>
   );
 };
 
