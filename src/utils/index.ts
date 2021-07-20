3b638537730a8e82560856a77939199a3909662d
en0c-026 <bitup.games@gmail.com>
2021-07-20 13:50:33 -0300

    add utils function for SettVaultsChart

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,3 +19,26 @@ export const arraySetValue = (arr: Array<boolean>, index: string, value: boolean
   arr[id] = value;
   return arr;
 };
+
+// A function that takes a number
+// Format the number, with a comma and with two decimal places
+// Returns the number formatted with the $ sign before the number
+export const formatNumber = (number: number): string => {
+  const formattedNumber: string = number
+    .toFixed(2)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return '$' + formattedNumber;
+};
+
+// A function that takes an array
+// Sum the value of the value property, of all the values of the array
+// Everything in a single line.
+export const arraySum = (arr: Array<any>): string => {
+  const number = arr.reduce((a, b) => a + b.value, 0);
+  return formatNumber(number);
+};
+
+export const kFormatt = (num: number) => {
+  return num > 999 ? num / 1000 + 'k' : num;
+};
