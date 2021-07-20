54766e3847142e033be290a07960dfe07c77645e
en0c-026 <bitup.games@gmail.com>
2021-07-20 13:53:27 -0300

    add ToolTipCard component, for SettVaultsChart main component

---
+++ src/components/Holdings/SettVaultsChart/ToolTipCard.tsx
@@ -0,0 +1,45 @@
+import { Card, Grid, Typography } from '@material-ui/core';
+import { FiberManualRecord } from '@material-ui/icons';
+import React, { memo } from 'react';
+import { useStyleVaultsChart } from '.';
+import { arraySum, formatNumber } from '../../../utils';
+
+const DotColorVault = (props: any) => {
+  const { color } = props;
+  return <FiberManualRecord style={{ color: color, paddingLeft: '4px', height: '8px', width: '8x' }} />;
+};
+
+const ToolTipCard = memo(({ active, payload, label }: any) => {
+  const classes = useStyleVaultsChart();
+  return (
+    <Card className={classes.toolTipCard}>
+      <Typography variant="body2">{label}</Typography>
+      {active &&
+        payload.map((item: any, index: any) => {
+          if (item.type !== 'none') {
+            return (
+              <Grid key={index} container justifyContent="space-between" alignItems="center">
+                <Typography variant="body2" color="primary">
+                  <DotColorVault color="#FFFFFF" />
+                  {item.name}
+                </Typography>
+                <Typography variant="body2" color="primary">
+                  {formatNumber(item.value)}
+                </Typography>
+              </Grid>
+            );
+          }
+        })}
+      <Grid container justifyContent="space-between" style={{ paddingTop: '16px' }}>
+        <Typography variant="body2" color="textPrimary">
+          Total
+        </Typography>
+        <Typography variant="body1" color="primary">
+          {active && arraySum(payload)}
+        </Typography>
+      </Grid>
+    </Card>
+  );
+});
+
+export default ToolTipCard;
