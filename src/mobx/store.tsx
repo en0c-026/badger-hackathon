cbc60ee135715055d0e3cbbef9bad3519ea369ae
en0c-026 <bitup.games@gmail.com>
2021-07-27 06:29:22 -0300

    general update and refactoring of the store

--- src/mobx/store.tsx
+++ src/mobx/store.tsx
@@ -1,32 +1,141 @@
-import { action, extendObservable } from 'mobx';
+import { computed, makeAutoObservable } from 'mobx';
 import { RouterStore } from 'mobx-router';
 import fetch from 'node-fetch';
 import { Account } from '../model/account.interface';
+import { SettVault } from '../model/sett-vault.interface';
+import { StrategyInfo } from '../model/strategy-info.interface';
 
 export class RootStore {
   private baseUrl = 'https://staging-api.badger.finance/v2';
   public router: RouterStore<RootStore>;
   public account?: Account;
+  public setts?: SettVault[];
+  public prices?: Record<string, number>;
+  public loading: boolean;
+  public networkError: boolean;
 
   constructor() {
     this.router = new RouterStore<RootStore>(this);
+    this.loading = true;
+    this.networkError = false;
+    makeAutoObservable(this);
 
-    extendObservable(this, {
-      account: this.account,
-    });
-
-    // load random test account
-    this.loadAccount('0x4e65175f05b4140a0747c29cce997cd4bb7190d4');
+    this.fetchData('0x4e65175f05b4140a0747c29cce997cd4bb7190d4');
   }
 
-  loadAccount = action(async (address: string): Promise<void> => {
-    const res = await fetch(`${this.baseUrl}/accounts/${address}`);
-    if (res.ok) {
-      this.account = await res.json();
-    }
-  });
-}
+  fetchData = async (address: string): Promise<void> => {
+    try {
+      console.log('Fetching account data for: ' + address);
+      const resAccount = await fetch(`${this.baseUrl}/accounts/${address}`);
+      const resSetts = await fetch(`${this.baseUrl}/setts/`);
+      const resPrices = await fetch(`${this.baseUrl}/prices/`);
 
+      if (resAccount.ok && resPrices.ok) {
+        const _account = await resAccount.json();
+        const _setts = await resSetts.json();
+        const _prices = await resPrices.json();
+
+        this.setData(_account, _setts, _prices);
+      }
+    } catch (error) {
+      this.setError(error);
+    }
+  };
+
+  setData = (_account: Account, _setts: SettVault[], _prices: Record<string, number>): void => {
+    this.account = _account;
+    this.setts = _setts;
+    this.prices = _prices;
+    this.loading = false;
+  };
+
+  setError = (error: Error): void => {
+    this.loading = false;
+    this.networkError = true;
+    console.error(error);
+  };
+
+  priceInBtc = (value: number): number => {
+    if (!this.prices) return 0;
+    return value / this.prices['0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'];
+  };
+
+  fetchVault = (id: string): SettVault => {
+    const vaultApr = this.setts?.find((sett) => sett.vaultToken === id);
+    return vaultApr as SettVault;
+  };
+
+  get earnedValuePercent(): number {
+    if (!this.account) {
+      return 0;
+    }
+    return (this.account.earnedValue / this.account.value) * 100;
+  }
+
+  get earnedBadgerValue(): number {
+    if (!this.account) {
+      return 0;
+    }
+    return this.account.balances.filter((balance) => balance.asset === 'BADGER')[0].earnedValue;
+  }
+
+  get claimableTotal(): number {
+    if (!this.account) {
+      return 0;
+    }
+    return this.account.claimableBalances.reduce((acc, cur) => acc + cur.balance, 0);
+  }
+
+  get claimable(): boolean {
+    if (!this.account) {
+      return false;
+    }
+    return this.account.claimableBalances.length > 0;
+  }
+
+  get totalValueSetts(): number {
+    if (!this.account) {
+      return 0;
+    }
+    return this.account.balances.reduce((acc, cur) => acc + cur.value, 0);
+  }
+
+  get StrategyInfo(): StrategyInfo[] {
+    if (!this.account || !this.account.balances) {
+      return [];
+    }
+    const total = this.account.balances.reduce((acc, cur) => acc + cur.value, 0);
+    return this.account.balances
+      .filter((item) => item.value > 0)
+      .map((item) => {
+        return {
+          name: item.asset,
+          balance: item.balance,
+          allocation: (item.value / total) * 100,
+          value: item.value,
+          valueInBtc: computed(() => this.priceInBtc(item.value / item.balance)).get(),
+          vault: computed(() => this.fetchVault(item.id)).get(),
+          settBalance: item,
+        };
+      })
+      .sort((a, b) => b.allocation - a.allocation);
+  }
+  // get AssetInfo(): Array<any> {
+  //   if (!this.account || !this.account.balances) {
+  //     return [];
+  //   }
+  //   const total = this.account.assetHoldings.reduce((acc, cur) => acc + cur.value, 0);
+  //   return this.account.assetHoldings
+  //     .filter((item) => item.value > 0)
+  //     .map((item) => {
+  //       return {
+  //         name: item.asset,
+  //         allocation: (item.value / total) * 100,
+  //       };
+  //     })
+  //     .sort((a, b) => b.allocation - a.allocation);
+  // }
+}
 const store = new RootStore();
 
 export default store;
