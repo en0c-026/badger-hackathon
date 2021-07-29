50fbdf8674ee276c3066349616a3ed86e4ad9c47
en0c-026 <bitup.games@gmail.com>
2021-07-29 04:03:10 -0300

    add theme constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,106 @@
+export const theme = {
+  palette: {
+    primary: {
+      main: '#FFFFFF',
+    },
+    secondary: {
+      main: '#747474',
+    },
+    text: {
+      primary: '#747474',
+      secondary: '#F2A627',
+    },
+    divider: '#111111',
+  },
+  typography: {
+    fontFamily: '"IBM Plex Sans", sans-serif',
+    h1: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '32px',
+      lineHeight: '40px',
+    },
+    h2: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '20px',
+      lineHeight: '28px',
+    },
+    h3: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '20px',
+      lineHeight: '28px',
+    },
+    h4: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    h5: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    h6: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    subtitle1: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '13px',
+      lineHeight: '21px',
+    },
+    subtitle2: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '13px',
+      lineHeight: '16.9px',
+    },
+    body1: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '21px',
+    },
+    body2: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '13px',
+      lineHeight: '21px',
+    },
+    caption: {
+      fontFamily: '"IBM Plex Mono", mono space',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '24px',
+      lineHeight: '40px',
+    },
+    overline: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '9px',
+      letterSpacing: '1px',
+    },
+  },
+};
+
 export type VaultPair = {
   id: string;
   name: string;
